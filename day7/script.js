39444da3b90593ebe80def69e4ac62ddd5ee1e66
hoskinsw <hoskinsw@email.sc.edu>
2026-09-22 14:21:59 -0400

    Finished javascript

--- day7/script.js
+++ day7/script.js
@@ -16,5 +16,12 @@ function takeDamage() {
     {
         healthText.innerText = 0;
         statusText.innerText = "Game Over!";
+        statusText.style.color = "#f9331d";
+        statusText.style.fontWeight = "bold";
+
+        document.body.style.backgroundColor = "#5a1a1a";
+
+        document.querySelector("button").disabled = true;
+        document.querySelector("button").innerText = "Dead";
     }
 }
